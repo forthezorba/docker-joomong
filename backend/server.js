ba6b2f64500f859703c19f7b6eb8c7b910b54ad5
forthezorba <forthezorba@gmail.com>
2020-10-01 15:07:48 +0900

    deploy app1-2

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const bodyParser = require("body-parser")
-
+const db = require('./db');
 const app = express()
 
 app.use(bodyParser.json);
@@ -9,35 +9,35 @@ db.pool.query(`CREATE TABLE lists (
     id INTEGER AUTO_INCREMENT,
     value TEXT,
     PRIMARY KEY (id)
-)`,(err,status,fields)=>{
-    console.log('results',results);
+)`, (err, status, fields) => {
+    console.log('results', results);
 })
 
 //DB to front
-app.get('/api/values',function(req,res){
+app.get('/api/values', function (req, res) {
     //GET FROM DB
     db.pool.query('SELECT * FROM lists;',
-        (err,results,fields)=>{
-            if(err)
+        (err, results, fields) => {
+            if (err)
                 return res.status(500).send(err)
             else
                 return res.json(results);
-    })
+        })
 })
 
 //
-app.post('/api/value',function(req,res,next){
+app.post('/api/value', function (req, res, next) {
     //
     db.pool.query(`INSERT INTO lists (value) VALUES("${req.body.value}")`,
-        (err,results,fields)=>{
-            if(err)
+        (err, results, fields) => {
+            if (err)
                 return res.status(500).send(err)
-            else    
-                return res.json({success:true, value: req.body.value})
-    })
+            else
+                return res.json({ success: true, value: req.body.value })
+        })
 })
 
 
-app.listen(5000,()=>{
+app.listen(5000, () => {
     console.log('app 5000 port start')
 }); 
